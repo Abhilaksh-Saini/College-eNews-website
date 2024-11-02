f654bed2e6e13b0e64ef491d5af7b421d8781245
Abhilaksh Saini <abhilaksh.s@gmail.com>
2024-11-02 14:30:05 +0530

    Final contact details added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import Header from './components/Header';
-// import FeaturedNews from './components/FeaturedNews';
-// import LatestNews from './components/LatestNews';
-// import Events from './components/Events';
-// import Footer from './components/Footer';
-
-
-// const App = () => { 
-//     return (
-//         <div className="App">
-//             <Header />
-//             <main className="container">
-//                 <FeaturedNews />
-//                 <LatestNews />
-//                 <Events />
-//             </main>
-//             <Footer />
-//         </div>
-//     );
-// };
-
-// export default App;
-
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
